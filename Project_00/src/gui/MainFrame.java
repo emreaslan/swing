c083e27cc9abebd47b603a9f6eb6ba939cc6b57b
Emre Sercan Aslan <emre55aslan@gmail.com>
2019-01-28 21:32:14 +0300

    Lecture 47

--- Project_00/src/gui/MainFrame.java
+++ Project_00/src/gui/MainFrame.java
@@ -20,6 +20,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.KeyStroke;
 
 import controller.Controller;
@@ -35,6 +36,7 @@ public class MainFrame extends JFrame {
 	private PrefsDialog prefsDialog;
 	private Preferences prefs;
 	private JSplitPane splitPane;
+	private JTabbedPane tabPane;
 	
 	public MainFrame() {
 		super("Hello World");
@@ -46,9 +48,14 @@ public class MainFrame extends JFrame {
 		formPanel = new FormPanel();
 		tablePanel = new TablePanel();
 		prefsDialog = new PrefsDialog(this);
-		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, formPanel, tablePanel);
+		tabPane = new JTabbedPane();
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, formPanel, tabPane);
 
 		splitPane.setOneTouchExpandable(true);
+		
+		tabPane.addTab("Person Database", tablePanel);
+		tabPane.addTab("Messages", textPanel);
+		
 		prefs = Preferences.userRoot().node("db");
 
 		controller = new Controller();
