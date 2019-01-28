2d623fc9c82e1752a490fd65d5f5f972d784e418
Emre Sercan Aslan <emre55aslan@gmail.com>
2019-01-28 20:56:26 +0300

    Lecture 44

--- Project_00/src/gui/Toolbar.java
+++ Project_00/src/gui/Toolbar.java
@@ -1,51 +1,65 @@
 package gui;
+
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 
 public class Toolbar extends JPanel implements ActionListener {
 	private JButton saveButton;
 	private JButton refreshButton;
-	
+
 	private ToolbarListener textListener;
-	
+
 	public Toolbar() {
 		setBorder(BorderFactory.createEtchedBorder());
-		
+
 		saveButton = new JButton("Save");
-		refreshButton = new JButton("Refresh");
+		saveButton.setIcon(createIcon("../images/Save16.gif"));
 		
+		refreshButton = new JButton("Refresh");
+		refreshButton.setIcon(createIcon("../images/Refresh16.gif"));
+
 		saveButton.addActionListener(this);
 		refreshButton.addActionListener(this);
-		
+
 		setLayout(new FlowLayout(FlowLayout.LEFT));
-		
+
 		add(saveButton);
 		add(refreshButton);
 	}
-	
+
+	private ImageIcon createIcon(String path) {
+		URL url = getClass().getResource(path);
+		ImageIcon icon = new ImageIcon(url);
+
+		if (url == null) {
+			System.err.println("Unable to load image: " + path);
+		}
+		return icon;
+	}
+
 	public void setToolbarListener(ToolbarListener listener) {
 		this.textListener = listener;
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		JButton clicked = (JButton)e.getSource();
-		
-		if(clicked == saveButton) {
-			if(textListener != null) {
+		JButton clicked = (JButton) e.getSource();
+
+		if (clicked == saveButton) {
+			if (textListener != null) {
 				textListener.saveEventOccured();
 			}
-		}
-		else if(clicked == refreshButton) {
-			if(textListener != null) {
+		} else if (clicked == refreshButton) {
+			if (textListener != null) {
 				textListener.refreshEventOccured();
 			}
 		}
-		
+
 	}
 }
